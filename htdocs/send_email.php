ea3a7d95a0db6e046867df9ad28ca614adf1e962
retronihon <retronihon80@gmail.com>
2025-08-09 12:04:29 +0900

    Update send_email.php

--- htdocs/send_email.php
+++ htdocs/send_email.php
@@ -1,8 +1,8 @@
+<?php
 require 'src/Exception.php';
 require 'src/PHPMailer.php';
 require 'src/SMTP.php';
 
-<?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
